cafefc25e30104cdc9e4ae830ea9ec44200b716d
Will Leinweber <will@bitfission.com>
2017-04-15 23:03:06 -0700

    close

--- a_spec.cr
+++ a_spec.cr
@@ -234,7 +234,9 @@ describe "grisu3" do
     point.should eq 10
   end
 
-  it "falure case" do
+  it "failure case" do
+    # grisu should not be able to do this number
+    # this number is reused to ensure the fallback works
     status, point, str = test_grisu 3.5844466002796428e+298
     status.should eq false
     str.should_not eq "35844466002796428"
@@ -267,6 +269,10 @@ private def test_str(s, file = __FILE__, line = __LINE__)
   end
 end
 
+private def test_pair(v : UInt64, str, file = __FILE__, line = __LINE__)
+  d = pointerof(v).as(Float64*).value
+  test_pair(d, str, file, line)
+end
 private def test_pair(v : Float64, str, file = __FILE__, line = __LINE__)
   float_to_s(v).should eq(str), file, line
 end
@@ -299,4 +305,33 @@ describe "to_s" do
   it { test_pair 1100.0, "1100.0" }
   it { test_pair 100000000000000.0, "100000000000000.0" }
   it { test_pair 1000000000000000.0, "1.0e+15" }
+
+  it "min float64" do
+    test_pair 5e-324, "5.0e-324"
+  end
+
+  it "max float64" do
+    test_pair 1.7976931348623157e308, "1.7976931348623157e+308"
+  end
+
+  it "large number, rounded" do
+    test_pair 4.1855804968213567e298, "4.185580496821357e+298"
+  end
+
+  it "small number, rounded" do
+    test_pair 5.5626846462680035e-309, "5.562684646268003e-309"
+  end
+
+  it "falure case" do
+    # grisu cannot do this number, so it should fall back to libc
+    test_pair 3.5844466002796428e+298, "3.5844466002796428e+298"
+  end
+
+  it "smallest normal" do
+    test_pair 0x0010000000000000_u64, "2.2250738585072014e-308"
+  end
+
+  it "largest denormal" do
+    test_pair 0x000FFFFFFFFFFFFF_u64, "2.225073858507201e-308"
+  end
 end
--- float_printer.cr
+++ float_printer.cr
@@ -2,6 +2,7 @@ require "./grisu3"
 
 module FloatPrinter
   extend self
+  BUFFER_SIZE = 128
 
   def fast_to_s(v : Float64, io : IO)
     d64 = IEEE.to_d64(v)
@@ -25,11 +26,19 @@ module FloatPrinter
   end
 
   def internal(v : Float64, io : IO)
-    buffer = StaticArray(UInt8, 128).new(0_u8)
+    buffer = StaticArray(UInt8, BUFFER_SIZE).new(0_u8)
     status, decimal_exponent, length = Grisu3.grisu3(v, buffer.to_unsafe)
-    point = decimal_exponent + length
 
-    # _str = String.new(buffer.to_unsafe); pp [v, _str, point, decimal_exponent, length ]
+    unless status
+      # grisu3 does not work for ~0.5% of floats
+      # when this happens, fallback to another, slower approach
+      LibC.snprintf(buffer.to_unsafe, BUFFER_SIZE, "%.17g", v)
+			len = LibC.strlen(buffer)
+      io.write_utf8 buffer.to_slice[0, len]
+      return
+    end
+
+    point = decimal_exponent + length
 
     exp = point
     exp_mode = point > 15 || point < -3
